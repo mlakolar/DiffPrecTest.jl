d4f927e7fd78a7f28cd5d43323785cbedec6ac73
mkolar <mkolar@mgpu01.chicagobooth.edu>
2019-07-05 12:21:32 -0500

    script for collecting results

---
+++ exper/collect.sh
@@ -0,0 +1,13 @@
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_1_1 sim1_res_1_1
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_1_2 sim1_res_1_2
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_1_3 sim1_res_1_3
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_2_1 sim1_res_2_1
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_2_2 sim1_res_2_2
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim1/res_2_3 sim1_res_2_3
+
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_1_1 sim2_res_1_1
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_1_2 sim2_res_1_2
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_1_3 sim2_res_1_3
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_2_1 sim2_res_2_1
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_2_2 sim2_res_2_2
+jl1 collect_results.jl /scratch/mkolar/diffTest/sim2/res_2_3 sim2_res_2_3
