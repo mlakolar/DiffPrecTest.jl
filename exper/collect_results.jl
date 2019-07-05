a08b093e7ea69fd7684720b699289e3a42e387f3
Mladen Kolar <mkolar@gmail.com>
2019-07-04 21:37:46 -0500

    collect results script

---
+++ exper/collect_results.jl
@@ -0,0 +1,26 @@
+using DiffPrecTest
+using JLD
+
+resPath = ARGS[1]
+outPrefix = ARGS[2]
+
+# resPath = /scratch/mkolar/diffTest/sim1/res_2_2_$(rep).jld
+
+NUM_REP = 1000
+results = Array{Any}(undef, 3, NUM_REP)
+for rep=1:NUM_REP
+    global results
+    fname = "$(resPath)_$(rep).jld"
+
+    # try
+      file = jldopen(fname, "r")
+      res = read(file, "est")
+      close(file)
+
+      results[:, rep] = deepcopy(res)
+    # catch
+    #     @show fname
+    # end
+end
+
+@save "$(outPrefix).jld" results
